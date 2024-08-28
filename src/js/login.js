100b70f0045b2563438ab719df66d39058ba330c
Ahmad Rifki Ayala <ayalarifki@gmail.com>
2024-08-28 15:41:54 +0700

    fetch login with email

--- src/js/login.js
+++ src/js/login.js
@@ -4,45 +4,44 @@ import { endpointAuth } from "./api.js"
 script.src = "https://accounts.google.com/gsi/client"
 
 export const login = async (req, res) => {
-    try {
-        const res = await postJSON(`${endpointAuth}`, {
-            body: JSON.stringify({
-                token: response.credential
-            }),
-        })
-        const data = await res.json();
-        if (res.ok){
-            console.log(data);
-            swal.fire({
-                icon: "success",
-                title: "Welcome to ULBI",
-                text: `hello, ${data.user}!`,
-                showConfirmButton: false,
-                timer: 2000
-            }).then(() => {
-                window.location.href = "https://login.ulbi.ac.id/auth/";
+        console.log("Encoded JWT ID token: " + res.credential);
+        try {
+            const result = await fetch(endpointAuth, {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ token: res.credential })
             });
-        } else {
-            if (res.status === 401) {
-                await swal.fire({
-                    icon: "error",
-                    title: "Authentication Failed",
-                    text: "Please check your credentials and try again.",
-                    showConfirmButton: true,
-                    confirmButtonText: "Try Again",
+            const data = await result.json();
+            if (res.ok) {
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Welcome to ULBI',
+                    text: `Hello, ${data.user}!`,
+                    showConfirmButton: false,
                     timer: 2000
                 }).then(() => {
-                    window.location.href = "/";
+                    window.location.href = "https://login.ulbi.ac.id/auth/";
                 });
+            } else {
+                if (res.status === 401) {
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Authentication Failed',
+                        text: 'Please check your credentials and try again.',
+                        showConfirmButton: true,
+                        confirmButtonText: 'Try Again',
+                        timer: 2000
+                    }).then(() => {
+                        window.location.href = "/";
+                    });
+                }
             }
+        } catch (error) {
+            console.error(error);
+            Swal.fire({
+                icon: 'error',
+                title: 'An Error Occurred',
+                text: 'An error occurred while trying to login. Please try again later.',
+            });
         }
-
-    } catch (error) {
-        console.error(error);
-        await swal.fire({
-            icon: "error",
-            title: "An Error Occurred",
-            text: "An error occurred while trying to login. Please try again later.",
-        });
     }
-}
