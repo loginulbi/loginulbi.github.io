826b0d64e1a75a43ea726b0536d37dadb911ea41
Ahmad Rifki Ayala <ayalarifki@gmail.com>
2024-09-19 19:14:10 +0700

    refactor: Add cookie.js for redirect functionality and improve error handling in wauth.js

---
+++ src/js/cookie.js
@@ -0,0 +1,19 @@
+import { CihuyGetCookie } from "https://c-craftjs.github.io/cookies/cookies.js";
+
+let redirectUrl = CihuyGetCookie("redirect")
+
+if (!redirectUrl) {
+    Swal.fire({
+        tittle: "info",
+        text: "Kakak pilih sistem nya dulu yaa",
+        icon: "info",
+        confirmButtonText: "OK",
+        allowOutsideClick: false, 
+        allowEscapeKey: false, 
+        allowEnterKey: false, 
+    }).then((result) => {
+        if (result.isConfirm) {
+            window.location.href = "http://login.ulbi.ac.id/page/";
+        }
+    })
+}
