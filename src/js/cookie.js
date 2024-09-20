bee948d0b17cf74fbc34ffbf9edb02600eb12f7e
Ahmad Rifki Ayala <ayalarifki@gmail.com>
2024-09-20 08:14:26 +0700

    refactor: Remove Swal.fire and update cookie.js for redirect functionality

--- src/js/cookie.js
+++ src/js/cookie.js
@@ -3,17 +3,5 @@ import { CihuyGetCookie } from "https://c-craftjs.github.io/cookies/cookies.js";
 let redirectUrl = CihuyGetCookie("redirect")
 
 if (!redirectUrl) {
-    Swal.fire({
-        tittle: "info",
-        text: "Kakak pilih sistem nya dulu yaa",
-        icon: "info",
-        confirmButtonText: "OK",
-        allowOutsideClick: false, 
-        allowEscapeKey: false, 
-        allowEnterKey: false, 
-    }).then((result) => {
-        if (result.isConfirmed) {
-            window.location.href = "http://login.ulbi.ac.id/page/";
-        }
-    })
+    window.location.href = "http://login.ulbi.ac.id/page/";
 }
